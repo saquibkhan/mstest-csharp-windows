9b0e870bb16c368617aec514db354229ebf652ad
Saquib Khan <saquibkhan@github.com>
2020-06-23 22:47:30 +0530

    Using Test Parameters

--- mstest-csharp-windows/UnitTest1.cs
+++ mstest-csharp-windows/UnitTest1.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting.Logging;
 
 namespace mstest_csharp_windows
 {
     [TestClass]
     public class UnitTest1
     {
+        [ClassInitialize]
+        static public void TestClassInitializer(TestContext context)
+        {
+            if (context.Properties["paramName"] != null)
+            {
+                string testParam = context.Properties["paramName"].ToString();
+                Logger.LogMessage(testParam);
+            }
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
@@ -13,16 +24,6 @@ namespace mstest_csharp_windows
 
         [TestMethod]
         public void TestMethod2()
-        {
-        }
-
-        [TestMethod]
-        public void TestMethod3()
-        {
-        }
-
-        [TestMethod]
-        public void TestMethod4()
         {
         }
     }
